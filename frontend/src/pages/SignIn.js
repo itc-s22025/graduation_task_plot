e03e6af415ef23579872e15753f2a5c10d1b8b09
s22025 <s22025@std.it-college.ac.jp>
2024-01-15 10:00:53 +0900

    signinページ

---
+++ frontend/src/pages/SignIn.js
@@ -0,0 +1,28 @@
+import s from '../styles/signIn.module.css'
+import Link from 'next/link'
+
+const SignIn = () => {
+    return(
+        <>
+            <main className={s.all}>
+                <div className={s.boxLarge}>
+                    <h1 className={s.SignIn}>SignIn</h1>
+                    <p><input type="text" placeholder= "User ID" className={s.box}/></p>
+                    <p><input type="password" minLength="8" placeholder="Password" required className={s.box}/></p>
+                    <p><input type="submit" value="SIGN IN" className={s.signin} /></p>
+                </div>
+                <div className={s.or}>
+                    <p></p>
+                    <p>OR</p>
+                </div>
+                <div className={s.boxLarge}>
+                    <Link href="/">
+                        <p><input type="submit" value="SIGN UP" className={s.signup} /></p>
+                    </Link>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default SignIn
