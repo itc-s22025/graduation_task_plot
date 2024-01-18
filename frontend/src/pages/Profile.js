ee18809eaa80811340606c0465ab21e6253b1326
s22025 <s22025@std.it-college.ac.jp>
2024-01-18 12:40:35 +0900

    レイアウト修正した

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,12 +2,16 @@ import Bio from "../../components/bio";
 import BioBar from "../../components/biobar";
 import Post from "../../components/post";
 import s from "../styles/profile.module.css"
+import FrameLayout from "../../components/frameLayout";
+import Header from "../../components/header";
 
 
 const Profile = () => {
     return(
         <>
-            <div className={s.all}>
+            <Header title="Profile"/>
+            <FrameLayout />
+            <div>
                 <Bio />
                 <BioBar />
                 <Post />
